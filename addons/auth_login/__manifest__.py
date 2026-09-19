8e7072bbc1b59567d6f7b29dde9be3634fd758ad
Hassan <moshkou2000@gmail.com>
2026-09-19 17:31:08 +0800

    init auth login

---
+++ addons/auth_login/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Auth Login",
+    "version": "19.0.1.0.2",
+    "category": "Website",
+    "summary": "Keep Odoo login pages on the standard web login layout",
+    "license": "LGPL-3",
+    "author": "Custom Development",
+    "depends": ["web", "website"],
+    "data": [
+        "views/login_layout.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
